463642b061c52448b3e575618cc5d5996dc5b947
bbougot <bbougot@gmail.com>
2015-04-15 10:22:09 +0200

    Add comments

--- TVShow/ViewModel/MoviesViewModel.cs
+++ TVShow/ViewModel/MoviesViewModel.cs
@@ -156,6 +156,7 @@ namespace TVShow.ViewModel
             // We stop any movie loading before searching action 
             StopLoadingMovies();
 
+            // Reset the token
             CancellationLoadingToken = new CancellationTokenSource();
             
             // We start from scratch : clean everything to not interfer with the results
@@ -191,6 +192,9 @@ namespace TVShow.ViewModel
             if ((String.IsNullOrEmpty(searchFilter) && String.IsNullOrEmpty(SearchMoviesFilter)) ||
                 (!String.IsNullOrEmpty(searchFilter) && !String.IsNullOrEmpty(SearchMoviesFilter)))
             {
+                // Reset the token
+                CancellationLoadingToken = new CancellationTokenSource();
+
                 // We update the current pagination
                 Pagination++;
 
